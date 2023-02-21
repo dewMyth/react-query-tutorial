c3611132ef4fd8e67ea1049744b21f535bfce972
Dewmith Akalanka <akalankadewmith@gmail.com>
2023-02-21 09:21:28 +0530

    Add custom useQuery hook

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 import React from "react";
-import { useQuery } from "react-query";
+import { useSupeheroesData } from "../hooks/useSuperHeroesData";
 
 const RQSuperHeroesPage = () => {
   const onSuccess = (data) => {
@@ -11,34 +10,11 @@ const RQSuperHeroesPage = () => {
     console.log("Error", error);
   };
 
-  const result = useQuery(
-    "super-heroes", //key
-    () => {
-      return axios.get("http://localhost:4000/superheroes");
-    }, //Promise
-    {
-      // cacheTime: 5000, //5 seconds cache => only keep cache for 5 seconds
-      // staleTime: 5000, //10 seconds stale => if cache is older than 10 seconds, then fetch again
-      // refetchOnMount: true, //Refetch on mount
-      // refetchOnWindowFocus: true, //Refetch on window focus => should click the tab to refetch
-      // refetchInterval: 1000, //Refetch every 5 seconds automatically - like real time
-      // refetchIntervalInBackground: true, //Refetch in background - Reftch even if the tab is not active
-      onSuccess: onSuccess,
-      onError: onError,
-    }
+  //we can destruct from useSuperHeroesData hook
+  const { isLoading, isFetching, error, isError, data } = useSupeheroesData(
+    onSuccess,
+    onError
   );
-
-  // This is the same as above, a common approach
-  // const fetchSuperHeroes = async () => {
-  //   return await axios.get("http://localhost:4000/superheroes");
-  // };
-  // const result = useQuery("super-heroes", fetchSuperHeroes);
-
-  // // Result is an object with load of pre-defined properties.
-  // console.log(result);
-
-  //we can destruct them
-  const { isLoading, isFetching, error, isError, data } = result;
 
   console.log("Loading...", isLoading);
   console.log("Fetching...", isFetching);
